4ada8e9722a602b6f73e96f16cef1fc03c852d00
BE02LEEJAEHOON <ljhx6787@naver.com>
2025-04-11 19:58:32 +0900

    chore : s3-development.py gdal 라이브러리 경로 설정

--- django/a_core/settings/s3-development.py
+++ django/a_core/settings/s3-development.py
@@ -109,3 +109,8 @@ SHELL_PLUS_IMPORTS = [
     "from django.conf import settings",
     "from django.core.cache import cache",
 ]
+
+
+# GDAL 라이브러리 경로 설정
+GDAL_LIBRARY_PATH = "/opt/homebrew/opt/gdal/lib/libgdal.dylib"
+GEOS_LIBRARY_PATH = "/opt/homebrew/opt/geos/lib/libgeos_c.dylib"

611e0b0dc2cd4e39dbfb907ee0d9b363dddbfc2b
BE02LEEJAEHOON <ljhx6787@naver.com>
2025-07-23 09:38:46 +0900

    fix: Channels 3.x에 맞게 JWTAuthMiddleware를 BaseMiddleware 상속 및 비동기 시그니처로 수정

--- django/a_apis/auth/jwt_middleware.py
+++ django/a_apis/auth/jwt_middleware.py
@@ -2,30 +2,18 @@ from urllib.parse import parse_qs
 
 import jwt
 from channels.db import database_sync_to_async
+from channels.middleware import BaseMiddleware
 
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AnonymousUser
 
 User = get_user_model()
 
 
-class JWTAuthMiddleware:
-    def __init__(self, inner):
-        self.inner = inner
-
-    def __call__(self, scope):
-        return JWTAuthMiddlewareInstance(scope, self.inner)
-
-
-class JWTAuthMiddlewareInstance:
-    def __init__(self, scope, inner):
-        self.scope = dict(scope)
-        self.inner = inner
-
-    async def __call__(self, receive, send):
-        headers = dict(
-            (k.decode(), v.decode()) for k, v in self.scope.get("headers", [])
-        )
+class JWTAuthMiddleware(BaseMiddleware):
+    async def __call__(self, scope, receive, send):
+        headers = dict((k.decode(), v.decode()) for k, v in scope.get("headers", []))
         token = None
         # 1. Authorization 헤더에서 토큰 추출
         auth_header = headers.get("authorization")
@@ -33,7 +21,7 @@ class JWTAuthMiddlewareInstance:
             token = auth_header.split(" ", 1)[1]
         # 2. 쿼리스트링에서도 토큰 허용 (옵션)
         if not token:
-            query_string = self.scope.get("query_string", b"").decode()
+            query_string = scope.get("query_string", b"").decode()
             params = parse_qs(query_string)
             token = params.get("token", [None])[0]
         user = await self.get_user(token)
@@ -41,36 +29,21 @@ class JWTAuthMiddlewareInstance:
 
         logger = logging.getLogger("chatroom_debug")
         logger.error("[JWTAuthMiddleware] user set: %s", user)
-        # scope 복사본에 user를 넣고 inner에 전달
-        scope = dict(self.scope)
         scope["user"] = user
-        inner = self.inner(scope)
-        return await inner(receive, send)
+        return await super().__call__(scope, receive, send)
 
     @database_sync_to_async
     def get_user(self, token):
         if not token:
-            from django.contrib.auth.models import AnonymousUser
-
             return AnonymousUser()
         try:
             payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
             user_id = payload.get("user_id")
             if not user_id:
-                from django.contrib.auth.models import AnonymousUser
-
                 return AnonymousUser()
             return User.objects.get(id=user_id)
         except Exception:
-            from django.contrib.auth.models import AnonymousUser
-
             return AnonymousUser()
-
-
-from asgiref.sync import sync_to_async
-
-# 미들웨어 스택으로 사용하기 위한 헬퍼
-from channels.middleware import BaseMiddleware
 
 
 def JWTAuthMiddlewareStack(inner):
